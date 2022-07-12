86a709036882d2e7b2e6d3edacbccaf1c123460f
Daniel Fireman <danielfireman@gmail.com>
2022-07-12 17:18:34 +0000

    Adiciona testes da raiz quadrada e organiza código

--- api/src/main/java/com/danielfireman/ifal/calcapi/App.java
+++ api/src/main/java/com/danielfireman/ifal/calcapi/App.java
@@ -5,12 +5,8 @@ import io.jooby.Jooby;
 public class App extends Jooby {
 
   {
-    get("/", ctx -> "Welcome to Jooby!");
-
-    get("/raiz/{op}", ctx -> {
-      float op = ctx.path("op").floatValue();
-      return Math.sqrt(op);
-    });
+    mvc(new Index());
+    mvc(new Raiz());
   }
 
   public static void main(final String[] args) {
